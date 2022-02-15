47c655641929113026aa5da74041233fe6195cf1
kclaudeeager <claudekwizera003@gmail.com>
2022-02-15 10:31:26 +0200

    Loading Suppliers

--- app/src/main/java/com/demo/makeorders/RetailerHome.java
+++ app/src/main/java/com/demo/makeorders/RetailerHome.java
@@ -10,10 +10,13 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -42,7 +45,10 @@ private String token;
 TextView userName;
 String all_names="";
 Button sendBtn;
-
+User user;
+    ArrayAdapter user_adapter;
+public  ArrayList<User> userArrayList;
+public static  View.OnClickListener onClickListener;
 Product_List_Adapter product_list_adapter;
 
 ArrayList<Product> productArrayList;
@@ -57,6 +63,7 @@ ArrayList<Product> productArrayList;
         supplierSpinner=findViewById(R.id.spinner_supplier);
         frameLayout=findViewById(R.id.frameLayout_picked_products);
         sendBtn=findViewById(R.id.sendOrder);
+        userArrayList=new ArrayList<>();
         Intent intent=getIntent();
         productArrayList=new ArrayList<>();
         String response=intent.getStringExtra("UserInfo");
@@ -75,6 +82,7 @@ ArrayList<Product> productArrayList;
             e.printStackTrace();
         }
         loadProducts();
+        loadSuppliers();
     }
     synchronized private void loadProducts(){
         final ProgressDialog progressDialog = new ProgressDialog(this);
@@ -167,7 +175,100 @@ ArrayList<Product> productArrayList;
         }.start();
 
     }
-    private void loadSuppliers(){
+
+    synchronized private void loadSuppliers(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(true);
+        progressDialog.setMessage("Please wait for loading....");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        new Thread() {
+            public void run() {
+                try {
+                    RequestQueue requestQueue = Volley.newRequestQueue(RetailerHome.this);
+                    int method=0;
+                    String url = "https://orderproductsapi.herokuapp.com/api/v1/User/";
+
+                    JSONObject js = new JSONObject();
+
+                    // Make request for JSONObject
+                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                            method, url,null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.i("Response obj: ",response.toString());
+                                    Map  UserInfo=new HashMap<>();
+                                    try {
+
+                                        UserInfo=toMap(response);
+                                        Log.i("User details:",UserInfo.toString());
+                                        JSONArray jsonArray=new JSONArray(response.get("Users").toString());
+                                        for(int i=0;i<jsonArray.length();i++){
+                                            user=new User();
+                                            JSONObject object=new JSONObject(jsonArray.getJSONObject(i).toString());
+                                            user.setCompany(object.getString("company"));
+                                            user.setEmail(object.getString("email"));
+                                            user.setRole(object.getInt("role"));
+                                            Log.i("User:>>",user.toString());
+                                            if(!userArrayList.contains(user)&& user.role==1)
+                                                userArrayList.add(user);
+                                        }
+                                        ArrayList<String> users=new ArrayList<>();
+                                        for (User user:userArrayList) {
+                                            users.add(user.getCompany());
+                                        }
+                                       user_adapter
+                                                = new ArrayAdapter(
+                                                RetailerHome.this,
+                                                android.R.layout.simple_spinner_item,
+                                               users);
+                                        supplierSpinner.setAdapter( user_adapter);
+                                        user_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                                        progressDialog.dismiss();
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //  parseVollyError(error);
+                            Log.i("error: ",""+error);
+                            progressDialog.dismiss();
+                        }
+                    }) {
+
+                        /**
+                         * Passing some request headers
+                         */
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            HashMap<String, String> headers = new HashMap<String, String>();
+                            headers.put("Content-Type", "application/json; charset=utf-8");
+                            return headers;
+                        }
+
+                    };
+
+                    // Adding request to request queue
+                    Volley.newRequestQueue(RetailerHome.this).add(jsonObjReq).setRetryPolicy(new DefaultRetryPolicy(0,-1,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT))
+                            .setShouldCache(false);
+
+                }
+
+                catch (Exception e) {
+                    Log.e("tag", ""+e.getMessage());
+                }
+                // dismiss the progress dialog
+
+
+                // finish();
+            }
+        }.start();
 
     }
+
+
 }
